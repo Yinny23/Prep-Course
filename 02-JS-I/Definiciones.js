f19f82955460acd927c2159cea44d0ab6cf8b90a
Yinny23 <yinny231@gmail.com>
2022-06-08 23:19:38 -0500

    homework 2

---
+++ 02-JS-I/Definiciones.js
@@ -0,0 +1,19 @@
+Definiciones
+Variable 
+//Una variable es una forma de almacenar el valor de algo para usar más tarde. 
+//Para crear una variable en JavaScript se usa la palabra clave var seguida de un espacio y el nombre de la variable. 
+Strings
+//Son una o varias palabras que se encierran entre comillas.
+Funciones
+//Son tipos especiales de objetos, llamados objetos invocables. 
+//Podemos pasarle argumentos a las funciones si agregamos una variable a los paréntesis cuando declaramos la función, podemos 
+//usar esta variable dentro de nuestra función. 
+//return: Cuando queramos devolver algo de la función se usa la declaración de retorno "return" como la única forma en que
+//los datos escapan de una función.
+Declaraciones
+//El control de flujo es una forma de que nuestra función verifique si algo es "true", y ejecute el código suministrado si es así o avance si no lo es. 
+//Para esto se usa la palabra clave if 
+Booleanos
+//Los Booleanos provienen de la lógica de Boole. Esencialmente significa que tiene dos opciones, activar o desactivar, 0 o 1, 
+//verdadero o falso. En Javascript usamos booleanos para significar verdadero o falso.
+//Los valores posibles de un dato Booleano en JS son: "true" o "false".
---
+++ 03-JS-II/Conceptos.js
@@ -0,0 +1,16 @@
+Conceptos
+
+//Bucles for: Permiten evaluar expresiones una y otra vez hasta que devuelve lo que estamos buscando o se detiene después de cierto tiempo.
+//Estos bucles tienen una sintaxis única, similar a la instrucción if, pero un poco más compleja. Necesitaremos tres cosas: 
+//Primero debemos declarar una variable, esto es sobre lo que se repetirá el bucle.
+//Segundo a través de una expresión condicional, el ciclo continuará sucediendo hasta que esta declaración sea "false"
+//Tercero, incrementamos nuestra variable.
+
+//&&: Es un operador lógico también conocido como "Y" ("AND"). Permite evaluar dos expresiones y devuelve verdadero si ambas expresiones son "true".
+//Si uno (o ambos) de ellos es falso, este operador devolverá "false".
+
+//||: Es un operador lógico también conocido como "Ó" ("OR"). Determina si una de las dos expresiones es "true" y devuelve "true" si una o (ambas) 
+//de las expresiones es "true". O devuelve "false" si ambas expresiones son "false".
+
+//!: Es un operador lógico también conocido como "NOT" ("NO"). Permite negar una expresión o igualdad, devuelven el valor booleano opuesto 
+//de lo que se le pasa. 
