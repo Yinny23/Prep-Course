a24c77f197a9195acafad8a1e4c214271fac4891
Yinny23 <yinny231@gmail.com>
2022-06-30 16:14:05 -0500

    homework JS-IV

---
+++ 05-JS-IV/Objetos.js
@@ -0,0 +1,12 @@
+//* Objetos: Son contenedores de datos que a diferencia de los arrays, contienen mucha información sobre una sola cosa. Estos se instancian usando llaves {}
+//* Propiedades: Las propiedades en los objetos usan un concepto llamado pares de clave:valor. La clave (key) es el identificador y el valor (value) 
+// es el valor que queremos guardar en esa clave. Los objetos pueden contener muchos pares de clave-valor. Las claves son únicas en un objeto, aunque,
+// varias claves pueden tener el mismo valor. Los valores pueden ser cualquier tipo de dato de Javascript; cadena, número, booleano, matriz, función 
+// o incluso otro objeto. 
+//* Métodos: En los objetos, los valores se pueden establecer en funciones. Las funciones guardadas en un objeto se denominan métodos. Se puede establecer una
+//clave para un nombre y el valor para una función. Se puede llamar a estos métodos usando notación de puntos y paréntesis finales. 
+//* Bucles for...in: Javascript tiene un segundo tipo de bucle for, diferente al bucle estándar, conocido como "for...in loop". Tiene una sintaxis que comienza 
+// igual que los bucles estándar, pero entre paréntesis se declara una variable, la palabra clave in y el nombre del objeto. Esto recorrerá cada clave del objeto 
+// y finalizará cuando se hayan iterado todas las claves.
+//* Notación de puntos vs notación de corchetes: Con la notación de puntos podemos llamar al nombre del objeto, un punto y el nombre de la clave. La notación de 
+// corchetes es como llamar a un elemento en una matriz, aunque con corchetes se debe usar una cadena o número, o una variable que apunte a una cadena o número 
