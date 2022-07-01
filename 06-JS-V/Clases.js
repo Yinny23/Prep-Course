9f9e54cbfe2efb69df278095e20b1d6717f9e8ae
Yinny23 <yinny231@gmail.com>
2022-07-01 17:00:47 -0500

    Homework JSV

---
+++ 06-JS-V/Clases.js
@@ -0,0 +1,7 @@
+//* prototype: Es una forma única de establecer un método una vez y dar acceso a cada objeto de esa clase a esos métodos. Cada clase tiene una propiedad
+//prototype, que luego podemos establecer en métodos, estos tienen acceso a la palabra clave this. 
+//* Constructors: Para entender que son los constructores es importante introducir el tema de la Herencia y Polimorfismo de las clases. Cuando hacemos referencia
+//a herencia nos referimos a la capacidad de un constructor de heredar propiedades y métodos de otro constructor. 
+// Cuando hablamos de Polimorfismo nos referimos a la capacidad de que objetos distintos puedan responder a un llamado igual de acuerdo a su propia naturaleza.
+// La Herencia en JS se maneja con prototipos que toman los métodos pasados por sus "padres" meduiante la Prototype Chain. Nosotros también podemos crear nuestros 
+// propios constructores de los cuales heredar. 
